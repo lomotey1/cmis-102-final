437968e4b85b275bde302fcb7048bcd2501faa13
David Lomotey <80100400+lomotey1@users.noreply.github.com>
2023-03-03 08:49:09 -0500

    Add files via upload

---
+++ FinalAssignment_Lomotey.py
@@ -0,0 +1,40 @@
+
+
+
+def Welcome():
+    print("CMIS 102 Final Assignment")
+
+def GetChosenService():
+    while True:
+        print("1: House Cleaning Service")
+        print("2: Yard Services")
+        print("0: Exit")
+
+        try:
+            userChoice = eval(input("Please enter your choice: "))
+        except:
+            print("invalid choice\n")
+            continue
+
+        if userChoice == 1 or userChoice == 2:
+            return userChoice
+        elif userChoice == 0:
+            print("Thank you for using this service.")
+            exit()
+        else:
+            continue
+
+
+
+def main(): 
+    #variables
+    totalCost = 0
+    userChoice = 0
+
+    Welcome()
+    userChoice = GetChosenService()
+    print (userChoice)
+
+    #if else for service
+
+main()
